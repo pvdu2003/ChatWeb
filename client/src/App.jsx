ccb9aaabeef93ef4481594610de5253d9ad9266a
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-08-03 16:26:14 +0700

    Add some basic UI for authentication-frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
